f544da80dbbc1765b047f5db4f033ac69ee90c5d
Maxim Tartachnik <maxim.tartachnik@gmail.com>
2017-04-14 11:45:28 +0300

    Added test project

---
+++ JimiSDKRxample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JimiSDKRxample
+//
+//  Created by Maxim Tartachnik on 4/14/17.
+//  Copyright © 2017 Maxim Tartachnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
